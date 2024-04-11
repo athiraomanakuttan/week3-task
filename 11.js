1868ff1689181f572149c3df17bbc099efeadacd
athira <athiraomanakuttan20@gmail.com>
2024-04-11 09:59:48 +0530

    11th programe

--- 11.js
+++ 11.js
@@ -3,16 +3,9 @@ document.getElementById('submitForm').addEventListener('submit', function (e) {
     let arrayLimit = document.getElementById('arrayLimit').value;
     let resultBox = document.getElementById('result')
     const arr1 = window.prompt("Enter " + arrayLimit + " elements of first array (Coma seperated)").split(',');
-    const arr2 = window.prompt("Enter " + arrayLimit + " elements of first array (Coma seperated)").split(',');
-    resultBox.innerHTML = "<p> before swap <br> <br> array1 : " + arr1 + "</p>";
-    resultBox.innerHTML += "<p> array2 : " + arr2 + "</p>";
-    for (var i = 0; i < arrayLimit; i++) {
-        let temp = arr1[i];
-        arr1[i] = arr2[i];
-        arr2[i] = temp;
-    }
-    resultBox.innerHTML += "<p> After swap <br> <br> array1 : " + arr1 + "</p>";
-    resultBox.innerHTML += "<p> array2 : " + arr2 + "</p>";
+    resultBox.innerHTML = "The array you entered : " + arr1 ;
+    let count=arr1.filter((value)=>value%2===0).reduce((counter,value)=>counter+1,0)
+    resultBox.innerHTML+="<br> Number of even numbers in the given array is  " +count
 })
 
 
